2b4e13330668e622357a6c1ab88747dc725dfcbd
Daft <daftdevs@gmail.com>
2022-10-22 22:14:04 +0700

    feat: create categories contant

---
+++ src/constants/categories.tsx
@@ -0,0 +1,27 @@
+export const CATEGORIES: {
+  [key: number]: {
+    id: number;
+    label: string;
+  };
+} = {
+  1: {
+    id: 1,
+    label: "Happiness & Mindfulness",
+  },
+  11: {
+    id: 11,
+    label: "Career & Business",
+  },
+  12: {
+    id: 12,
+    label: "Productivity & Time Management",
+  },
+  19: {
+    id: 19,
+    label: "Society & Politics",
+  },
+  21: {
+    id: 21,
+    label: "Investment & Finance",
+  },
+};
